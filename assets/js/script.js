f786b232de0469fac8f4140e821eaa9c5df5840b
BillyGrind <denayeranthony623@gmail.com>
2023-05-03 12:19:50 +0200

    exercice 2 done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 import { findTimeZone } from "./exercice1.js";
-import { daysPassedSinceMyBirth } from "./exercice2.js";
+import { daysPassedSinceMyBirth, daysSinceDate } from "./exercice2.js";
 
 
 findTimeZone();
 daysPassedSinceMyBirth();
+daysSinceDate();
